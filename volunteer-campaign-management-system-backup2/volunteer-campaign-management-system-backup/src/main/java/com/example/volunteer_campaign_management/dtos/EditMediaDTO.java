8711499c383cf49166b986c11ba5e8640d62038f
ngocntbhe153486@fpt.edu.vn <125891947+baongoccuti@users.noreply.github.com>
2023-12-11 13:00:05 +0700

    BE

---
+++ volunteer-campaign-management-system-backup2/volunteer-campaign-management-system-backup/src/main/java/com/example/volunteer_campaign_management/dtos/EditMediaDTO.java
@@ -0,0 +1,30 @@
+package com.example.volunteer_campaign_management.dtos;
+
+public class EditMediaDTO {
+    private int idMedia;
+    private String image;
+    private String video;
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getId() {
+        return idMedia;
+    }
+
+    public void setId(int id) {
+        this.idMedia = id;
+    }
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+}
